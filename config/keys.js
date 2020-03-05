fed44c19b00cb99c22208f3a173b786cb61bf0ad
ogata216 <amidstasinglebreath@gmail.com>
2020-03-05 01:40:24 -0800

    mern-auth test

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://chcollab3:redemptioniscostly1135@ds241308.mlab.com:41308/heroku_ntfbtfk7",
+    secretOrKey: "secret"
+  };
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const http = require('http').Server(app);
+const path = require('path');
+//const io = require('socket.io')(http);
+
+const passport = require("passport");
+const users = require("./routes/api/users");
+
+const uri = process.env.MONGODB_URI;
+
+const Message = require('./models/Messages');
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
