5666ddbe5f1e800b574649cdebf8d532667f3016
ogata216 <amidstasinglebreath@gmail.com>
2020-03-05 21:43:07 -0800

    yet another try

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://<authtests>:<ccollvid135>@ds145178.mlab.com:45178/heroku_gv49wn5q",
+    mongoURI: "mongodb://authtests:ccollvid135@ds145178.mlab.com:45178/heroku_gv49wn5q",
     secretOrKey: "secret"
   };
