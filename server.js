a5c6928b466771bdd157f20b29d45b0afaa47438
ogata216 <amidstasinglebreath@gmail.com>
2020-03-05 21:04:58 -0800

    deploy test 2

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
 );
 app.use(bodyParser.json());
 // DB Config
-const db = process.env.mongoURI || require("./config/keys").mongoURI;
+const db = process.env.MONGODB_URI || require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -34,7 +34,8 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
-app.use("/",(req, res) => {res.send('Root,')})
+app.use(express.static("client/build/"));
+app.use("/",(req, res) => {res.send('Root,')});
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
